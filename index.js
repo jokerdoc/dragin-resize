b76e6afdf37af0b1b2e93fe5a3f1ead643bb7154
九林 <jokerdoc@163.com>
2020-04-09 15:18:06 +0800

    版本更新

--- index.js
+++
@@ -1,2 +0,0 @@
-import DraginResize from './lib/dragin-resize';
-export default DraginResize;
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import scss from 'rollup-plugin-scss';
 
@@ -7,13 +8,14 @@ export default {
   output: {
     file: './dist/dragin-resize.js',
     format: 'umd',
-    name: 'DraginResize'
+    name: 'joDraginResize'
   },
   external: ['jo-util', 'jo-dragin'],
   plugins: [
     resolve({
       jsnext: true
     }),
+    commonjs(),
     scss({
       output: 'dist/dragin-resize.css'
     }),
